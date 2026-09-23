818776322de6ee8256a0d096c6845fee6925b878
Israel <iaguilera.gan@gmail.com>
2026-09-22 23:19:36 -0300

    Se actualiza home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import Header from './Header';
 import CardPizza from './Cardpizza';
-import espanola from './src/assets/img/espanola'
 import { formatNumber } from '../utils/format';
 
 const Home = () => {
